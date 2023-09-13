37543a80f42c5579a81a8e4149a0366476242cc7
Aidah Islam <islamaidah369@gmail.com>
2023-09-12 22:01:55 -0400

    first commit

---
+++ src/JavaPractice/HelloWorld.java
@@ -0,0 +1,17 @@
+package JavaPractice; //first create a package
+
+public class HelloWorld { //under package, create a class (can have many classes)
+
+	public static void main(String[] args) { //this is the main method
+		// TODO Auto-generated method stub
+System.out.println("Welcome class of August 2023"); // print line method (does it mean we can have method inside the method?) text has to be inside the double quote
+System.out.print("In this class, we'll learn about Java. "); //as you can see, I keep a space before the last double quote. it actually works as a space in output.
+System.out.println("Java is a programming language"); //if use print instead of println, it won't print a new line.
+System.out.println(3); //don't need double quote to print numbers.
+System.out.println(3+3);
+
+	} /*curly bracket after class and method, each method opens and closes with curly bracket and has first bracket, 
+	; after each line except class + main method */
+	
+
+} 
